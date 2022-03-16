55418695f7b7cca12c61ff5e65329b3eb432d34b
Ronen8686 <101619546+Ronen8686@users.noreply.github.com>
2022-03-16 22:03:37 +0530

    Update Chitoge.ts

--- src/commands/General/Chitoge.ts
+++ src/commands/General/Chitoge.ts
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `🌟 *Chitoge* 🌟\n\n🍀 *Description: A WhatsApp Bot With Rich Anime Features.*\n\n🌐 *URL: https://github.com/ShineiIchijo/Chitoge* \n\n 📒 *Guide: https://github.com/ShineiIchijo/Chitoge-Guides* \n`,
+				caption: `🌟 *Elaina* 🌟\n\n🍀 *Description: A WhatsApp Bot With Rich Anime Features.*\n\n🌐 *URL: https://github.com/Ronen8686/Chitoge* \n\n 📒 *Guide: https://github.com/Ronen8686/Chitoge-Guides* \n`,
 			}
 		);
 	};
